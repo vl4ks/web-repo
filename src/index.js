29a125ca5d7076b0fc37f96d6455adfac1e6280c
vl4ks <veli4koksenia@yandex.ru>
2024-12-17 15:14:24 +0500

    Практика5-fix

--- src/index.js
+++ src/index.js
@@ -2,30 +2,21 @@
 //     console.log(document.getElementById('test-id'))
 // })
 
-const menuToggleSmall = document.getElementById('menu-toggle-small');
-const menuToggleMedium = document.getElementById('menu-toggle-medium');
-const menuClose = document.getElementById('menu-close');
-const dropdownMenu = document.getElementById('dropdown');
+const toggleButton1 = document.getElementById('toggle-button-1');
+const toggleButton2 = document.getElementById('toggle-button-2');
+const hiddenBlock = document.getElementById('hidden-block');
+const closeButton = document.getElementById('close-button');
 
+// Показ скрытого блока
+toggleButton1.addEventListener('click', () => {
+    hiddenBlock.style.display = 'block';
+});
 
-const openMenu = () => {
-    dropdownMenu.style.display = 'block'; // Показываем блок
-    document.body.style.overflow = 'hidden'; // Блокируем прокрутку
-};
+toggleButton2.addEventListener('click', () => {
+    hiddenBlock.style.display = 'block';
+});
 
-const closeMenu = () => {
-    dropdownMenu.style.display = 'none'; // Скрываем блок
-    document.body.style.overflow = ''; // Возвращаем прокрутку
-};
-
-//события
-menuToggleSmall?.addEventListener('click', openMenu);
-menuToggleMedium?.addEventListener('click', openMenu);
-menuClose?.addEventListener('click', closeMenu);
-
-// Закрытие меню при клике вне его области
-dropdownMenu.addEventListener('click', (event) => {
-    if (event.target === dropdownMenu) {
-        closeMenu();
-    }
+// Закрытие скрытого блока
+closeButton.addEventListener('click', () => {
+    hiddenBlock.style.display = 'none';
 });
